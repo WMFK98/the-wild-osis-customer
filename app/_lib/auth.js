b661edbe39ced44d0ef5867253bd2beb619f8d2e
realFK98 <wishira2013.coth@gmail.com>
2024-09-24 01:19:13 +0700

    add server action for sign in and sing out

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,9 +8,19 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECREAT,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+import { auth } from '../_lib/auth';
 
 export const metadata = {
   title: 'Guest area',
 };
 
-export default function Home() {
-  return <div>Your Account</div>;
+export default async function Home() {
+  const session = await auth();
+  const firstname = session.user.name.split(' ').at(0);
+  return (
+    <div className="font-semibold text-2xl text-accent-400 md-7">
+      Welcome, {firstname}
+    </div>
+  );
 }
