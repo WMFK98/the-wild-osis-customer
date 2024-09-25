315bad434b23fd492b3b6c46deb0c227a2ba4205
realFK98 <wishira2013.coth@gmail.com>
2024-09-25 13:13:28 +0700

    add update profile and delete reservation with server action!

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,9 @@
 'use server';
 
-import { signIn, signOut } from './auth';
+import { revalidatePath } from 'next/cache';
+import { auth, signIn, signOut } from './auth';
+import { supabase } from './supabase';
+import { getBookings } from './data-service';
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
@@ -8,4 +11,47 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: '/' });
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error('You are not allowed to delete this booking');
+
+  const { error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId);
+  if (error) {
+    console.error(error);
+    throw new Error('Booking could not be deleted');
+  }
+  revalidatePath('/account/reservations');
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+  if (/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error('Please provied a valid national ID');
+
+  const updateDate = { nationality, countryFlag, nationalID };
+
+  console.log(updateDate);
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateDate)
+    .eq('id', session.user.guestId);
+
+  if (error) throw new Error('Guest could not be updated');
+
+  revalidatePath('/account/profile');
 }
